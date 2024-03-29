32469ea4886f1f1229d9c27d7efe52221c7d5648
ainzdeveloper <158009579+ainzdeveloper@users.noreply.github.com>
2024-03-29 17:30:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -650,50 +650,43 @@ app.get("/eaaaay/api", (req, res) => {
   });
 });
 
-console.log("/ainz/api?username=&password=")
-app.get("/ainz/api", (req, res) => {
-  const access_token = "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32";
-  const username = req.query.username;
-  const password = req.query.password;
+console.log("/auth/login?username=&password=")
+app.get('/auth/login', (req, res) => {
+    const { email, password } = req.query;
 
-  if (!username || !password) {
-    return res.send({ message: "Both username and password are required" });
-  }
+    const par = {
+        "adid": "e3a395f9-84b6-44f6-a0ce-fe83e934fd4d",
+        "email": email,
+        "password": password,
+        "format": "json",
+        "device_id": "67f431b8-640b-4f73-a077-acc5d3125b21",
+        "cpl": "true",
+        "family_device_id": "67f431b8-640b-4f73-a077-acc5d3125b21",
+        "locale": "en_US",
+        "client_country_code": "US",
+        "credentials_type": "device_based_login_password",
+        "generate_session_cookies": "1",
+        "generate_analytics_claim": "1",
+       "generate_machine_id": "1",
+        "currently_logged_in_userid": "0",
+        "irisSeqID": "1",
+        "try_num": "1",
+        "enroll_misauth": "false",
+        "meta_inf_fbmeta": "NO_FILE",
+        "source": "login",
+        "machine_id": "KBz5fEj0GAvVAhtufg3nMDYG",
+        "fb_api_req_friendly_name": "authenticate",
+        "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+        "api_key": "882a8490361da98702bf97a021ddc14d",
+        "access_token": "350685531728|62f8ce9f74b12f84c123cc23437a4a32"
+    };
 
-  const params = {
-    format: "json",
-    generate_session_cookies: "1",
-    generate_machine_id: "1",
-    generate_analytics_claim: "1",
-    device_id: "yrcyg4m1-o7m5-pghw-atiu-n04mh4nlka6n",
-    email: username,
-    password: password,
-    locale: "en_US",
-    client_country_code: "US",
-    credentials_type: "device_based_login_password",
-    fb_api_caller_class: "com.facebook.account.login.protocol.Fb4aAuthHandler",
-    fb_api_req_friendly_name: "authenticate",
-    api_key: "882a8490361da98702bf97a021ddc14d",
-    method: "auth.login",
-    access_token: access_token,
-  };
-
-  request.get({ url: api_url, qs: params }, (error, response, body) => {
-    if (error) {
-      return res.send({ message: "Internal server error" });
-    }
-
-    const responseJson = JSON.parse(body);
-
-    if (responseJson) {
-      return res.send({
-        access_token: responseJson.access_token,
-        session_cookies: responseJson.session_cookies,
-      });
-    } else {
-      return res.send({ message: "Wrong Credentials" });
-    }
-  });
+    request.get({ url: "https://b-api.facebook.com/method/auth.login", qs: par }, (error, response, body) => {
+        if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+        res.json(JSON.parse(body));
+    });
 });
 
 console.log("/gen")
